dadf03fcfafe77187af963b9ac94eca24e45bd27
BenColligan <bencolligan@live.com>
2017-07-11 21:55:40 +0100

    Add files via upload

---
+++ howfastabc v0.1.py
@@ -0,0 +1,13 @@
+from datetime import datetime
+
+print("Welcome to How Fast ABC, Type START to begin... ")
+openingResponse = input()
+
+if openingResponse == "START":
+	{
+	print("Time has started... BEGIN TYPING THE ALPHABET!")
+	}
+elif openingResponse != "START" or "start":
+	{
+	print("This is not a known command, type START to begin... ")
+	}
